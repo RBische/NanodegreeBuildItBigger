1dfd98da3877fa6c879679149c5cc5429523a6e3
Raphaël Bischof <raphael.bischof@gmail.com>
2015-08-25 21:51:30 +0200

    feat: Java library

--- app/src/main/java/fr/bischof/raphael/builditbigger/MainActivityFragment.java
+++ app/src/main/java/fr/bischof/raphael/builditbigger/MainActivityFragment.java
@@ -5,18 +5,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import fr.bischof.raphael.JokeSmith;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    @Bind(R.id.btnJavaLib)
+    Button mBtnJavaLib;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBtnJavaLib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), JokeSmith.getARandomJoke(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- jokerlib/src/main/java/fr/bischof/raphael/JokeSmith.java
+++ jokerlib/src/main/java/fr/bischof/raphael/JokeSmith.java
@@ -1,4 +1,20 @@
 package fr.bischof.raphael;
 
+import java.util.Random;
+
 public class JokeSmith {
+
+    private static String[] jokes = {
+            "Hide and seek champion : \";\" since 1958",
+            "Algorithm (noun.) : Word used by programmers when they do not want to explain what they did.",
+            "Hardware (noun.) : The part of a computer that you can kick.",
+            "Real programmers count from 0.",
+            "Chuck Norris writes code that optimizes itself",
+            "A SQL query walks into a bar, walks up to two tables and asks : \"Can I join you ?\""
+    };
+
+    public static String getARandomJoke(){
+        Random randomGenerator = new Random();
+        return jokes[randomGenerator.nextInt(5)];
+    }
 }
