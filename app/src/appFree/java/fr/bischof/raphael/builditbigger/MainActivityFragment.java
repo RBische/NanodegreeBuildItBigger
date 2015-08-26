57dfe9f6959c16b742f45bee4fdd25bfb5895d29
rbischof <raphael.bischof@gmail.com>
2015-08-26 11:53:58 +0200

    feat: Interstitial ad + Progress indicator

--- app/src/appFree/java/fr/bischof/raphael/builditbigger/MainActivityFragment.java
+++ app/src/appFree/java/fr/bischof/raphael/builditbigger/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package fr.bischof.raphael.builditbigger;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,8 +22,12 @@ import fr.bischof.raphael.jokereader.JokeReaderActivity;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private String joke;
     @Bind(R.id.btnGCE)
     Button mBtnGCE;
+    private InterstitialAd mInterstitialAd;
+    private ProgressDialog progress;
+
     public MainActivityFragment() {
     }
 
@@ -35,16 +42,45 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+            }
+
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                showJoke();
+            }
+        });
+
+        requestNewInterstitial();
         mBtnGCE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (progress==null){
+                    progress = new ProgressDialog(getActivity());
+                }
+                progress.setMessage(getString(R.string.loading_joke));
+                progress.show();
                 JokeRetriever retriever = new JokeRetriever();
                 retriever.setOnJokeLoadedListener(new JokeRetriever.OnJokeLoadedListener() {
                     @Override
                     public void onJokeLoaded(String joke) {
-                        Intent intent = new Intent(getActivity(),JokeReaderActivity.class);
-                        intent.putExtra(JokeReaderActivity.EXTRA_JOKE,joke);
-                        startActivity(intent);
+                        if (getActivity()!=null){
+                            progress.dismiss();
+                            progress = null;
+                            MainActivityFragment.this.joke = joke;
+                            if (mInterstitialAd.isLoaded()) {
+                                mInterstitialAd.show();
+                            } else {
+                                showJoke();
+                            }
+                        }
                     }
                 });
                 retriever.execute(getActivity());
@@ -58,5 +94,19 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+    }
+
+    private void showJoke(){
+        Intent intent = new Intent(getActivity(),JokeReaderActivity.class);
+        intent.putExtra(JokeReaderActivity.EXTRA_JOKE,joke);
+        startActivity(intent);
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
     }
 }
